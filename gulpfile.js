94ef437f5ffbfcc7d4d024b94b82fb66d0a048f0
zakuni <kunio038@gmail.com>
2014-03-15 21:37:35 +0900

    use gulp-coffee-lint stop using gulp-notify-growl

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var notify  = require('gulp-notify');
-var growl = require('gulp-notify-growl');
 
 var coffee = require('gulp-coffee');
-
-var growlNotifier = growl({
-  hostname : 'localhost' // IP or Hostname to notify, default to localhost
-});
+var coffeelint = require('gulp-coffeelint');
 
 var paths = {
   coffee: ['*.coffee']
@@ -17,16 +13,23 @@ gulp.task('coffee', function() {
   gulp.src(paths.coffee)
     .pipe(coffee({sourceMap: true})
       .on('error', gutil.log)
-      .on('error', notify.onError({
-        notifier: growlNotifier
-      }))
+      .on('error', notify.onError())
     )
     .pipe(gulp.dest('./'))
-    .pipe(notify({notifier: growlNotifier}));
+    .pipe(notify());
+});
+
+gulp.task('lint', function () {
+  gulp.src(paths.coffee)
+    .pipe(coffeelint())
+    .pipe(coffeelint.reporter())
+    .pipe(notify())
+    .pipe(coffeelint.reporter('fail'))
+      .on('error', notify.onError())
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.coffee, ['coffee']);
 });
 
-gulp.task('default', ['coffee', 'watch']);
+gulp.task('default', ['coffee', 'lint', 'watch']);
