945fbe17860bc47dd8ac93f64b9c51accedad97c
zakuni <kunio038@gmail.com>
2014-03-15 22:08:28 +0900

    rewrite with coffee

--- train-train.coffee
+++ train-train.coffee
@@ -1,27 +1,24 @@
-`
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+scene = new THREE.Scene()
+camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xcceeff, 1);
-document.body.appendChild(renderer.domElement);
+renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setClearColor(0xcceeff, 1)
+document.body.appendChild(renderer.domElement)
 
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+geometry = new THREE.CubeGeometry(1,1,1)
+material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
-camera.position.z = 5;
+camera.position.z = 5
 
-var render = function () {
-  requestAnimationFrame(render);
+render = () ->
+  requestAnimationFrame(render)
 
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  cube.rotation.x += 0.1
+  cube.rotation.y += 0.1
 
-  renderer.render(scene, camera);
-};
+  renderer.render(scene, camera)
 
-render();
-`
+render()
--- train-train.js
+++ train-train.js
@@ -1,32 +1,38 @@
 (function() {
+  var camera, cube, geometry, material, render, renderer, scene;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  scene = new THREE.Scene();
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xcceeff, 1);
-document.body.appendChild(renderer.domElement);
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  renderer = new THREE.WebGLRenderer();
 
-camera.position.z = 5;
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-var render = function () {
-  requestAnimationFrame(render);
+  renderer.setClearColor(0xcceeff, 1);
 
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  document.body.appendChild(renderer.domElement);
 
-  renderer.render(scene, camera);
-};
+  geometry = new THREE.CubeGeometry(1, 1, 1);
 
-render();
-;
+  material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+  });
 
+  cube = new THREE.Mesh(geometry, material);
+
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  render = function() {
+    requestAnimationFrame(render);
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+    return renderer.render(scene, camera);
+  };
+
+  render();
 
 }).call(this);
 
