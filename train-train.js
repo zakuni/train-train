b95d4ae305e9cb9ec420dc365b5b146b35dea89c
zakuni <kunio038@gmail.com>
2014-03-16 23:21:37 +0900

    add shadow

--- train-train.js
+++ train-train.js
@@ -11,15 +11,23 @@
 
   renderer.setClearColor(0xcceeff, 1);
 
+  renderer.shadowMapEnabled = true;
+
   document.body.appendChild(renderer.domElement);
 
-  light = new THREE.AmbientLight(0x505050);
+  light = new THREE.AmbientLight(0x808080);
+
+  light.castShadow = true;
 
   scene.add(light);
 
   directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 
   directionalLight.position.set(0, 100, 50);
+
+  directionalLight.castShadow = true;
+
+  directionalLight.shadowCameraVisible = true;
 
   scene.add(directionalLight);
 
@@ -33,19 +41,23 @@
 
   ground.rotation.x = Math.PI / -2;
 
+  ground.receiveShadow = true;
+
   scene.add(ground);
 
   geometry = new THREE.CubeGeometry(3, 1, 1);
 
   material = new THREE.MeshLambertMaterial({
-    color: 0x777777,
+    color: 0xeeeeff,
     transparent: true,
-    opacity: 0.5
+    opacity: 0.1
   });
 
   train = new THREE.Mesh(geometry, material);
 
   train.position.set(0, 0.5, 0);
+
+  train.castShadow = true;
 
   scene.add(train);
 
@@ -57,11 +69,15 @@
 
   building = new THREE.Mesh(geometry, material);
 
-  building.position.set(0, 0.5, -5);
+  building.position.set(0, 3, -5);
+
+  building.castShadow = true;
+
+  building.receiveShadow = true;
 
   scene.add(building);
 
-  camera.position.set(0, 2, 5);
+  camera.position.set(0, 8, 12);
 
   render = function() {
     requestAnimationFrame(render);
